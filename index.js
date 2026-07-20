4ff5bb54f56cd50c64417a9a7cb8b149ce7bdf47
Alameen <Adekualameem@gmail.com>
2026-07-19 20:40:38 -0400

    Get requests

--- index.js
+++ index.js
@@ -5,7 +5,18 @@ const port = 3000;
 app.get('/',(req, res) => {
     res.send('Hello World');
 });
-
+app.get('/tasks',(req, res) => {
+    res.send({
+        'name': 'Tasks API',
+        'version': '1.0',
+        'endpoints': ['/tasks'],
+    })
+})
+app.get('/health', (req, res) => {
+    res.send({
+        'status': 'ok'
+    })
+})
 app.listen(port, () => {
     console.log(`Example app testing on port ${port}`)
     console.log(`View here: http://localhost:3000/`);
