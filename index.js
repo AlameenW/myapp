1fb5c48bbb10ada1c9c001f9c9f8b4bb0fb7c8de
Alameen <Adekualameem@gmail.com>
2026-07-19 22:19:26 -0400

    Full crud

--- index.js
+++ index.js
@@ -64,8 +64,30 @@ app.post('/tasks',(req, res) => {
 })
 
 // Update
+app.put('/tasks/:id', (req, res) => {
+    const id = req.params.id;
+    const updates = req.body;
 
+    const cur_task = tasks.find(task => task.id == id)
+    if (!cur_task){
+        return res.status(404).json({error: 'Task does not exist'})
+    }
+    if (!updates.title){
+        return res.status(404).json({error: 'Title is empty'})
+    }
+
+    cur_task.title = updates.title
+    return res.status(200).json({message: 'Task updated successfully', task: cur_task})
+})
 // Delete
+app.delete('/tasks/:id', (req, res) => {
+    const id = req.params.id;
+    const del_index = tasks.findIndex(task => task.id == id);
+    if (del_index == -1)
+        return res.status(404).json({ error: "Tasks does not exist" });
+    const [deleted] = tasks.splice(del_index,1);
+    res.status(200).json({message: 'task deleted successfully', task: deleted});
+})
 app.listen(port, () => {
     console.log(`Example app testing on port ${port}`)
     console.log(`View here: http://localhost:3000/`);
