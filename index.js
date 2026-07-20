ec62921f4ecaed5c0ce4530ed6f3cd6dfe106e58
Alameen <Adekualameem@gmail.com>
2026-07-19 21:12:36 -0400

    Read endpoints with 404

--- index.js
+++ index.js
@@ -2,15 +2,41 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const tasks = [
+    {
+        id: 1,
+        title: 'task1',
+        done: 'true'
+    },
+    {
+        id: 2,
+        title: 'task2',
+        done: 'false'
+    },
+    {
+        id: 3,
+        title: 'task3',
+        done: 'false'
+    }
+]
 app.get('/',(req, res) => {
-    res.send('Hello World');
-});
-app.get('/tasks',(req, res) => {
     res.send({
         'name': 'Tasks API',
         'version': '1.0',
         'endpoints': ['/tasks'],
     })
+})
+app.get('/tasks', (req, res) => {
+    res.send(tasks)
+})
+app.get('/tasks/:id', (req, res) => {
+    const id = req.params.id;
+    const task = tasks.find(task => task.id == id)    
+    if (!task){
+        res.status(404).send({ error: `Task ${id} not found` });
+        return;
+    }
+    res.status(200).send(task)
 })
 app.get('/health', (req, res) => {
     res.send({
