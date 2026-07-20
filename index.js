1acae4e382bfba8bb19d112d3b448d8a9cc31664
Alameen <Adekualameem@gmail.com>
2026-07-19 21:41:17 -0400

    Create added with validation

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+app.use(express.json());
+
 
 const tasks = [
     {
@@ -43,6 +45,27 @@ app.get('/health', (req, res) => {
         'status': 'ok'
     })
 })
+
+
+// Post
+app.post('/tasks',(req, res) => {
+    req_task = req.body
+    if (!req_task.title){
+        return res.status(404).send({error: 'Task name missing'});
+    }
+    const new_id = tasks[tasks.length-1].id + 1
+    const new_task = {
+        id: new_id,
+        task: req_task.title,
+        done: 'false'
+    }
+    tasks.push(new_task)
+    res.status(200).send({message: 'Task added successfully'})
+})
+
+// Update
+
+// Delete
 app.listen(port, () => {
     console.log(`Example app testing on port ${port}`)
     console.log(`View here: http://localhost:3000/`);
