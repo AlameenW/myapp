c6e6d3a6db50ddc205a027264b370f10d76ba2cd
Alameen <Adekualameem@gmail.com>
2026-08-01 20:24:09 -0500

    Swagger ui implemented

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
+
 app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 
 const tasks = [
@@ -52,6 +56,9 @@ app.post('/tasks',(req, res) => {
     req_task = req.body
     if (!req_task.title){
         return res.status(404).send({error: 'Task name missing'});
+    }
+    if (typeof(req_task.done) != Boolean){
+        return res.status(404).send({error: 'Add a valid task status'})
     }
     const new_id = tasks[tasks.length-1].id + 1
     const new_task = {
